c17ba484a3824d741376d9dd9c891ac2ded2747c
karu007 <hemdevkaran07@gmail.com>
2018-03-04 00:13:54 +0530

    Update Scheduling_FCFS.java

--- Scheduling_FCFS.java
+++ Scheduling_FCFS.java
@@ -64,7 +64,7 @@ class SchedulingFunctionsClass implements Comparable<SchedulingFunctionsClass>
 	//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 }
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-class Scheduling
+class Scheduling_FCFS
 {
 	public static void main(String args[])
 	{
